0833beb7fbc6f6b1aaced6d238d2b826725f5854
markyboi246 <mpham27@student.gsu.edu>
2025-11-06 18:20:05 -0500

    connect Firebase and navigate to InventoryHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,21 +15,10 @@ class InventoryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Inventory Management App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const InventoryHomePage(),
-    );
-  }
-}
-
-class InventoryHomePage extends StatelessWidget {
-  const InventoryHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Inventory Management')),
-      body: const Center(child: Text('Firebase successfully initialized!')),
     );
   }
 }
